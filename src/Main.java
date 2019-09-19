f4c60ec9d72bd6048730fcc18d72f998be067eab
kouamax15 <kouamax15@gmail.com>
2019-09-19 15:55:19 -0400

    Resume making

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    private String email;
+    private String jobTitle;
+    private String companyName;
+    private
+    private String jobDescription;
+
+
+}
+
+
